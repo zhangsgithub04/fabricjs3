afba97280e9c022a8ca713c30e860be0cc6a7361
zhangsgithub04 <ssen_zhang@hotmail.com>
2024-03-09 06:32:03 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+fabric.CustomObject = fabric.util.createClass(fabric.Object, {
+    type: 'customobject',
+    
+    initialize: function () {
+    },
+    
+    _render: function (ctx) {
+    }
+});
