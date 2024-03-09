e29c1e3661b8dc37f6a1c1ceeec23db97e5177e0
zhangsgithub04 <ssen_zhang@hotmail.com>
2024-03-09 12:37:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
-fabric.CustomObject = fabric.util.createClass(fabric.Object, {
-    type: 'customobject',
-    
-    initialize: function (options) {
-        options = options || {};
-        
-        this.callSuper('initialize', options);
-    }
+// Wait for the fabric library to load
+  document.addEventListener("DOMContentLoaded", function() {
+
+    // Initialize canvas with Fabric.js
+    var canvas = new fabric.Canvas('myCanvas');
+/*
+    // Create a rectangle object
+    var rect = new fabric.Rect({
+      left: 100, // Distance from left of canvas
+      top: 100, // Distance from top of canvas
+      fill: 'red',
+      width: 200,
+      height: 100
+    });
+
+    // Add the rectangle to the canvas
+    canvas.add(rect);
+  */
+
+  
 
     
-    _render: function (ctx) {
-    }
+fabric.BitmapText.fromURL('https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png', function (text) {
+  canvas.add(text);
+}, {
+  text: 'Example text'
 });
+   
+  });
+   
