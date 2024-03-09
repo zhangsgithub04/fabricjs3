12308aa3d39c3bc00400d03d22ab9a0fdc19d0a3
zhangsgithub04 <ssen_zhang@hotmail.com>
2024-03-09 12:36:32 -0500

    Update BitmapText.js

--- BitmapText.js
+++ BitmapText.js
@@ -46,41 +46,13 @@ fabric.BitmapText = fabric.util.createClass(fabric.Image, {
   }
 });
 
-
-  // Wait for the fabric library to load
-  document.addEventListener("DOMContentLoaded", function() {
-
-    // Initialize canvas with Fabric.js
-    var canvas = new fabric.Canvas('myCanvas');
-/*
-    // Create a rectangle object
-    var rect = new fabric.Rect({
-      left: 100, // Distance from left of canvas
-      top: 100, // Distance from top of canvas
-      fill: 'red',
-      width: 200,
-      height: 100
-    });
-
-    // Add the rectangle to the canvas
-    canvas.add(rect);
-  */
-
-    fabric.BitmapText.fromURL = function (url, callback, options) {
+  fabric.BitmapText.fromURL = function (url, callback, options) {
   fabric.util.loadImage(url, function (img) {
     callback(new fabric.BitmapText(img, options));
   }, null, options && options.crossOrigin);
 };
 
-    
-fabric.BitmapText.fromURL('https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png', function (text) {
-  canvas.add(text);
-}, {
-  text: 'Example text'
-});
-   
-  });
-   
+  
 
 
 
